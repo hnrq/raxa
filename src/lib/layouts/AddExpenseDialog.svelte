8c7a134be095af497b358997e18d62dd8045b27a
Henrique Ramos <henriquealberone@outlook.com>
2024-10-19 16:03:25 -0300

    feat: create AddExpenseDialog layout

---
+++ src/lib/layouts/AddExpenseDialog.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Dialog from '$lib/components/Dialog.svelte';
+
+	let { open, onSubmit, participants } = $props<{
+		open: boolean;
+		onSubmit: () => void;
+		participants: string[];
+	}>();
+</script>
+
+<Dialog {open}></Dialog>
