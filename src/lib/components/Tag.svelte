948c0c8498f436ac0446c94807eff5c5b775367c
Henrique Ramos <henriquealberone@outlook.com>
2024-10-19 16:03:15 -0300

    feat: create Tag component

---
+++ src/lib/components/Tag.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	let { label, onRemove } = $props<{ label: string; onRemove: () => void }>();
+</script>
+
+<div class="tag">
+	<span>{label}</span>
+	<button onclick={onRemove}>x</button>
+</div>
