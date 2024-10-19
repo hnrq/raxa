81547c5eb8d829b2afc35e38b560a656db6ebac0
Henrique Ramos <henriquealberone@outlook.com>
2024-10-19 16:03:10 -0300

    feat: create Expense component

---
+++ src/lib/components/Expense.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import Tag from './Tag.svelte';
+
+	let { title, amount, paidBy, participants, onAddUser } = $props<{
+		name: string;
+		amount: number;
+		paidBy: string;
+		participants: Set<string>;
+		onAddUser: () => void;
+	}>();
+
+	const onRemove = (user: string) => participants.delete(user);
+</script>
+
+<div class="expense">
+	<h3>{title}</h3>
+	<p>Amount: {amount}</p>
+	<p>Paid by: {paidBy}</p>
+	<div class="expense__used-by">
+		Used by:
+		{#each participants as participant}
+			<Tag label={participant} onRemove={() => onRemove(participant)} />
+		{/each}
+	</div>
+	<button onclick={onAddUser}>Add user</button>
+</div>
