5a872d3f1bc33f9c9af8beaa004a1125341631c5
Henrique Ramos <henriquealberone@outlook.com>
2024-10-19 16:03:02 -0300

    feat: create Dialog component

---
+++ src/lib/components/Dialog.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	let { open, children } = $props<{ open: boolean }>();
+
+	let dialog: HTMLDialogElement;
+
+	$effect(() => {
+		if (open) dialog.showModal();
+		else dialog.close();
+	});
+</script>
+
+<dialog bind:this={dialog}>
+	<button
+		onclick={() => {
+			open = false;
+		}}>Close</button
+	>
+	{@render children()}
+</dialog>
