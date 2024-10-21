ac845ac74913e68aac9a66cf19ccb21cf52b06c8
Henrique Ramos <henriquealberone@outlook.com>
2024-10-21 12:15:42 -0300

    chore: move ParticipantsForm to a separate component

---
+++ src/lib/forms/ParticipantsForm.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	let {
+		onsubmit,
+		initialValue,
+		oncancel
+	}: {
+		onsubmit: (event: SubmitEvent) => void;
+		initialValue?: string;
+		oncancel: () => void;
+	} = $props();
+</script>
+
+<form {onsubmit}>
+	<textarea
+		placeholder="Type participants separated by comma"
+		name="participants"
+		value={initialValue}
+		required
+	></textarea>
+	<button onclick={oncancel}>Cancel</button>
+	<button type="submit">Save</button>
+</form>
