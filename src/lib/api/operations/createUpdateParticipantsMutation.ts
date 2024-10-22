053a3b460cc2df9ac111d4ee9802c09e229f25a3
Henrique Ramos <henriquealberone@outlook.com>
2024-10-21 23:32:28 -0300

    fix: cascade update expense participants when updating bill participants

--- src/lib/api/operations/createUpdateParticipantsMutation.ts
+++ src/lib/api/operations/createUpdateParticipantsMutation.ts
@@ -13,9 +13,33 @@ const createUpdateParticipantsMutation = (
 ) => {
 	const queryClient = useQueryClient();
 
+	const cascadeUpdateExpenseParticipants = async (id: string, participants: string[]) => {
+		const { expenses } = queryClient.getQueryData<Bill>(['bills', id])!;
+
+		const editedExpenses = expenses
+			.map((expense) => {
+				const remainingParticipants = expense.participants.filter((participant) =>
+					participants.includes(participant)
+				);
+				if (remainingParticipants.length === expense.participants.length) return undefined;
+
+				updateDoc(doc(db, 'bills', id, 'expenses', expense.id), {
+					participants: remainingParticipants
+				});
+				return { ...expense, participants: remainingParticipants };
+			})
+			.filter(Boolean);
+
+		if (editedExpenses.length > 0)
+			queryClient.setQueryData(['bills', id], (prev: Bill) => ({
+				...prev,
+				expenses: editedExpenses
+			}));
+	};
+
 	return createMutation({
 		...opts,
-		mutationKey: ['add-expense'],
+		mutationKey: ['update-participants'],
 		mutationFn: async ({ id, participants }: UpdateParticipantsMutationOptions) => {
 			updateDoc(doc(db, 'bills', id), { participants: [...new Set(participants)] });
 		},
@@ -24,6 +48,7 @@ const createUpdateParticipantsMutation = (
 				...prev,
 				participants: variables.participants
 			}));
+			cascadeUpdateExpenseParticipants(variables.id, variables.participants);
 			queryClient.invalidateQueries({ queryKey: ['bills', variables.id], exact: true });
 			opts?.onSuccess?.(data, variables, context);
 		}
