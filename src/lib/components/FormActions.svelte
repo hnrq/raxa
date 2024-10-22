bdfb75b4824b5b93dd6c2e24a8b693c4945deb90
Henrique Ramos <henriquealberone@outlook.com>
2024-10-21 22:36:01 -0300

    chore: create FormActions component

---
+++ src/lib/components/FormActions.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	const {
+		oncancel,
+		saveLabel = 'Save',
+		cancelLabel = 'Cancel'
+	}: { oncancel: () => void; saveLabel?: string; cancelLabel?: string } = $props();
+</script>
+
+<div class="form-actions">
+	<button type="reset" onclick={oncancel}>{cancelLabel}</button>
+	<button type="submit">{saveLabel}</button>
+</div>
+
+<style>
+	.form-actions {
+		display: flex;
+		gap: var(--base-spacing);
+	}
+</style>
