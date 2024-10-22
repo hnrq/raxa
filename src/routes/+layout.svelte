9be2578608b4f173920286c10f5e9c8f18f1a762
Henrique Ramos <henriquealberone@outlook.com>
2024-10-21 23:47:13 -0300

    feat: submit forms with ctrl+enter

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,23 @@
-<script>
+<script lang="ts">
+	import { browser } from '$app/environment';
 	import client from '$lib/api/client';
 	import '$lib/theme/index.css';
 	import { QueryClientProvider } from '@tanstack/svelte-query';
+	import { onMount } from 'svelte';
 
 	const { children } = $props();
+
+	onMount(() => {
+		if (browser)
+			document.body.addEventListener('keydown', (e: KeyboardEvent) => {
+				if (!(e.key === 'Enter' && (e.metaKey || e.ctrlKey)) || !e.target) return;
+
+				if ('form' in e.target) {
+					const formElement = e.target.form as HTMLFormElement;
+					formElement?.requestSubmit();
+				}
+			});
+	});
 </script>
 
 <QueryClientProvider {client}>
